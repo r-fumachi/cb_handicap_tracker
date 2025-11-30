dcf7fe292a11304d21c8e221373307cec57ed583
r-fumachi <127866411+r-fumachi@users.noreply.github.com>
2025-11-30 07:54:19 +0000

    uid rerun

--- commons.py
+++ commons.py
@@ -34,21 +34,27 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 def get_user_id():
-    if "uid" not in st.session_state:
-        uid = st_javascript("""
-            (async () => {
-                let id = window.localStorage.getItem('user_id');
-                if (!id) {
-                    id = crypto.randomUUID();
-                    window.localStorage.setItem('user_id', id);
-                }
-                return id;
-            })()
-        """)
+# If the UID was already obtained, use it
+    if "uid" in st.session_state:
+        return st.session_state["uid"]
 
-        if uid and uid != 0:
-            st.session_state.uid = uid
-            logger.info(uid)
-            st.rerun()
-    return uid
+    # Ask JavaScript to return the UID
+    uid = st_javascript("""
+        (async () => {
+            let id = localStorage.getItem('user_id');
+            if (!id) {
+                id = crypto.randomUUID();
+                localStorage.setItem('user_id', id);
+            }
+            return id;
+        })();
+    """)
 
+    # Streamlit will return "0" before JavaScript finishes.
+    # Ignore that value and wait for correct UID.
+    if not uid or uid == 0 or uid == "0":
+        st.stop()  # <-- STOP HERE and wait for next rerun
+
+    # Now JS has returned an actual UID
+    st.session_state["uid"] = uid
+    st.rerun()  # <-- restart app with UID loaded
